8184b1d70db190600648ca6ea6476541535d0f67
Sal <32248675+salauddinaliahmed@users.noreply.github.com>
2018-11-07 11:16:21 -0500

    Update LCD_V2.py
    bug with the function name is now fixed.

--- LCD_V2.py
+++ LCD_V2.py
@@ -23,7 +23,7 @@ def rcb_logo():
 
 # Drawing on screen
 def draw_onscreen(x, y, text):
-    a = IP_Addr()
+    a = ip_addr()
     if text == a:
         with canvas(device) as draw:
             draw.text((0,0), "IP:"+ IP, fill="white")
@@ -32,7 +32,7 @@ def draw_onscreen(x, y, text):
         with canvas(device) as draw:
             draw.text((0,0), "Internet offline", fill="white")
     else:
-        a = IP_Addr()
+        a = ip_addr()
         with canvas(device) as draw:
             #if "192.168.1" in a:
              #   a = "Incorrect Router"
