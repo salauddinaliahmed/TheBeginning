3622e6dc6f7632a51f6dcf7cadbe88ad4ed41518
Sal <32248675+salauddinaliahmed@users.noreply.github.com>
2018-12-18 09:18:20 -0500

    Uploaded linux installer files

--- Quadcopter/installer.sh
+++ Quadcopter/installer.sh
@@ -1,113 +1,102 @@
 #!/bin/bash
-while true
+while true  # Part 1 of the script
 do
-    echo Before we proceed with the installation, Checking internet connectivity...
+    echo Checking internet connectivity before proceeding with the installation:
     wget -q --spider https://google.com
     if [ $? -eq 0 ]; then
+	cd
+	declare -i err
+	err=0
         echo Connected
         #sudo apt-get update 
         #sudo apt-get upgrade
+        echo Installer [Part 1/Part 2]
+        echo This installer script will install all the necessary tools and dependencies. 
         echo -------------------------------------------------------------
         echo Enabling SSH
         sudo systemctl enable ssh 
-        echo SSH enabled
-        
-        echo -------------------------------------------------------------
-        echo Installing Python libraries
-        pip install pyserial
-        pip install dronekit 
-        sudo apt-get install libxml2-dev libxslt-dev 
-        sudo apt-get install libjpeg8-dev
-        pip install MAVProxy
-        pip install luma.core
-        pip install luma.oled 
-        pip install Pillow
-        sudo apt-get install python-dev python-opencv python-wxgtk3.0 python-matplotlib python-pygame python-lxml python-yaml
-        sudo apt-get install screen 
-        echo Python libraries downloaded 
-        
-        echo -------------------------------------------------------------
-        echo Installing node.js
-        cd
-        sudo apt-get update
-        sudo apt-get dist-upgrade
-        wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-arm-pi.tar.gz
-        cd /usr/local
-        sudo tar -xzf ~/node-v0.10.28-linux-arm-pi.tar.gz --strip=1
-        export NODE_PATH=”/usr/local/lib/node_modules”
-        echo Testing node with -v
-        node -v
-        echo Installing Cloud9 IDE
-        cd
-        git clone git://github.com/c9/core.git c9sdk
-        cd c9sdk
-        scripts/install-sdk.sh
-        
-        echo -------------------------------------------------------------
-        echo Setting up interfaces...
-        cd
-        sudo sed -i '$s/$/\nenable_uart=1/' /boot/config.txt
-        sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt
-        sudo sed -i 's/console=serial0,115200//g' /boot/cmdline.txt
-        echo i2c and serial port enabled. 
-        
-        echo -------------------------------------------------------------
-        echo Cloning RCBenchmark repositories and changing working directory.
-        cd
-        cd Transport_Protocol
-        sudo git checkout Transport_Protocol-V2
-        sudo git pull
-
-        echo -------------------------------------------------------------
-        echo Creating auto launching scripts
-        cd
-        mkdir autolaunch_scripts
-        cd
-        (crontab -l ; echo "@reboot screen -d -m -L /home/pi/autolaunch_scripts/mavproxy.log sh /home/pi/autolaunch_scripts/launch_mavproxy_rcb.sh") | sort - | uniq - | crontab -
-        sleep 0.1
-        (crontab -l ; echo "@reboot screen -d -m -L /home/pi/autolaunch_scripts/pose.log sh /home/pi/autolaunch_scripts/launch_pose_forward_rcb.sh") | sort - | uniq - | crontab -
-        sleep 0.1
-        (crontab -l ; echo "@reboot screen -d -m -L sh /home/pi/autolaunch_scripts/launch_OLED_screen.sh") | sort - | uniq - | crontab -
-        sleep 0.1
-        (crontab -l ; echo "@reboot screen -d -m -L sh /home/pi/autolaunch_scripts/launch_c9.sh") | sort - | uniq - | crontab -
-        echo Setting up autolaunch_scripts folder
-        sudo cp Transport_Protocol/Quad_Environment/launch_mavproxy_rcb.sh /home/pi/autolaunch_scripts
-        sudo cp Transport_Protocol/Quad_Environment/launch_pose_forward_rcb.sh /home/pi/autolaunch_scripts
-        sudo cp Transport_Protocol/Quad_Environment/launch_OLED_screen.sh /home/pi/autolaunch_scripts
-        sudo cp Transport_Protocol/Quad_Environment/launch_c9.sh /home/pi/autolaunch_scripts
-        echo Setup Complete! 
-        sleep 0.2
-        echo -------------------------------------------------------------
-        echo Making Files executable
-        sudo chmod +x ~/autolaunch_scripts/launch_mavproxy_rcb.sh
-        sudo chmod +x ~/autolaunch_scripts/launch_pose_forward_rcb.sh
-        sudo chmod +x ~/autolaunch_scripts/launch_OLED_screen.sh
-        sudo chmod +x ~/autolaunch_scripts/launch_c9.sh
-        echo Creating log deleting scripts
-        #sudo sed -i '$i \rm ~/Desktop/pose.log' /etc/r.local
-        #sudo sed -i '$i \rm ~/Desktop/mavproxy.log' /etc/rc.local
-        echo -------------------------------------------------------------
-        echo Cloning and Installing DronecodeSDK
-        cd && git clone https://github.com/dronecode/DronecodeSDK.git
-        sed -i '\|with vision system|,/}/ s|^|//|'  DronecodeSDK/plugins/action/action_impl.cpp
-        cd DronecodeSDK
-        git submodule update --init --recursive
-        make default 
-        sudo make default install
-        sudo ldconfig && cd 
-        echo Dronecode cloned and installed.
-        echo -------------------------------------------------------------
-        echo Installing Cmake
-        sudo apt-get install cmake 
-        echo -------------------------------------------------------------
-        echo All dependencies installed
-        echo Preparing to reboot 
-        echo you have 5 seconds to cancel. to cancel press Ctl+C
-        sleep 5
-        echo rebooting... 
-        sleep 1
-        sudo reboot 
-        break
+        err=err+$?
+	echo SSH enabled
+	echo -------------------------------------------------------------
+    echo Setting up interfaces...
+    cd
+    sudo sed -i '$s/$/\nenable_uart=1/' /boot/config.txt
+    err=err+$?
+	sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt
+    err=err+$?
+	sudo sed -i 's/console=serial0,115200//g' /boot/cmdline.txt
+    err=err+$?
+	echo i2c and serial port enabled. 
+	echo -------------------------------------------------------------
+    echo Installing Python libraries
+    pip install pyserial
+    err=err+$?
+	pip install dronekit 
+        err=err+$?
+    echo "The following libraries will take a upto 20 mins to complete installation."
+    echo ------------------------------------------------------------- 
+	sudo apt-get -y install libxml2-dev libxslt-dev 
+        err=err+$?
+	sudo apt-get -y install libjpeg8-dev
+        err=err+$?
+	pip install MAVProxy
+        err=err+$?
+	pip install Pillow
+        err=err+$?
+	pip install luma.oled 
+        err=err+$?
+	pip install luma.core
+        err=err+$?
+	sudo apt-get -y install python-dev python-opencv python-wxgtk3.0 python-matplotlib python-pygame python-lxml python-yaml
+        err=err+$?
+	sudo apt-get -y install screen 
+        echo $?
+	err=err+$?
+	echo Python dependencies downloaded 
+        echo $err
+	if [ $err -gt 0 ];
+	then
+	echo Error with Dronekit or SSH installation.
+	break
+	fi
+    echo -------------------------------------------------------------
+    echo Installing node.js
+    cd
+    sudo apt-get -y update
+    err=err+$?
+	sudo apt-get -y dist-upgrade
+    err=err+$?
+	wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-arm-pi.tar.gz
+    err=err+$?
+	cd /usr/local
+    sudo tar -xzf ~/node-v0.10.28-linux-arm-pi.tar.gz --strip=1
+    err=err+$?
+	export NODE_PATH=”/usr/local/lib/node_modules”
+    echo Testing node with -v
+    err=err+$?
+	node -v
+    echo Installing Cloud9 IDE
+    cd
+    git clone git://github.com/c9/core.git c9sdk
+    cd c9sdk
+    scripts/install-sdk.sh
+    err=err+$?
+    if [ $err -gt 0 ];
+	then
+	echo Error installing NodeJS or Cloud9
+	break
+	fi
+    while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) sh /home/pi/Transport_Protocol/Quad_Environment/installer_part2.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+    done
+    date > /home/pi/Created_on.txt
+    echo "Environment created. Check Created_on.txt for details."
+    echo 
     else 
         echo Please connect to wifi and run the script.
     fi
---
+++ Quadcopter/installer_part2.sh
@@ -0,0 +1,88 @@
+     echo -------------------------------------------------------------
+        echo Cloning RCBenchmark repositories and changing working directory.
+        cd
+        cd /home/pi/Transport_Protocol
+        sudo git checkout Transport_Protocol-V2
+        err=err+$?
+	sudo git pull
+	err=err+$?
+        echo -------------------------------------------------------------
+           echo Creating auto launching scripts
+        cd
+        mkdir autolaunch_scripts
+        cd
+        (crontab -l ; echo "@reboot screen -d -m -L /home/pi/autolaunch_scripts/mavproxy.log sh /home/pi/autolaunch_scripts/launch_mavproxy_rcb.sh") | sort - | uniq - | crontab -
+        err=err+$?
+	sleep 0.1
+	(crontab -l ; echo "@reboot screen -d -m -L /home/pi/autolaunch_scripts/pose.log sh /home/pi/autolaunch_scripts/launch_pose_forward_rcb.sh") | sort - | uniq - | crontab -
+        err=err+$?
+	sleep 0.1
+	(crontab -l ; echo "@reboot screen -d -m sh /home/pi/autolaunch_scripts/launch_OLED_screen.sh") | sort - | uniq - | crontab -
+        err=err+$?
+	sleep 0.1
+        (crontab -l ; echo "@reboot screen -d -m sh /home/pi/autolaunch_scripts/launch_c9.sh") | sort - | uniq - | crontab -
+        echo Setting up autolaunch_scripts folder
+        sudo cp /home/pi/Transport_Protocol/Quad_Environment/launch_mavproxy_rcb.sh /home/pi/autolaunch_scripts
+        err=err+$?
+	sudo cp /home/pi/Transport_Protocol/Quad_Environment/launch_pose_forward_rcb.sh /home/pi/autolaunch_scripts
+	err=err+$?
+        sudo cp /home/pi/Transport_Protocol/Quad_Environment/launch_OLED_screen.sh /home/pi/autolaunch_scripts
+        err=err+$?
+	sudo cp /home/pi/Transport_Protocol/Quad_Environment/launch_c9.sh /home/pi/autolaunch_scripts
+        err=err+$?
+	echo Setup Complete! 
+        sleep 0.2
+        if [ $err -gt 0 ];
+	then
+	echo RCB_Transport_Protocol installation failed. 
+	break
+	fi
+	echo Making Files executable
+        sudo chmod +x ~/autolaunch_scripts/launch_mavproxy_rcb.sh
+        err=err+$?
+	sudo chmod +x ~/autolaunch_scripts/launch_pose_forward_rcb.sh
+        sudo chmod +x ~/autolaunch_scripts/launch_OLED_screen.sh
+        err=err+$?
+	sudo chmod +x ~/autolaunch_scripts/launch_c9.sh
+        err=err+$?
+	echo Creating log deleting scripts
+        sudo sed -i '$i \rm ~/Desktop/pose.log' /etc/rc.local
+        err=err+$?
+	sudo sed -i '$i \rm ~/Desktop/mavproxy.log' /etc/rc.local
+        cd && sudo apt-get -y install curl
+	sudo apt-get -y install libcurl4-openssl-dev
+	err=err+$?
+	echo ----------------------------------------------------------------
+        echo Cloning and Installing DronecodeSDK
+        cd && git clone https://github.com/dronecode/DronecodeSDK.git
+        sed -i '\|with vision system|,/}/ s|^|//|'  DronecodeSDK/plugins/action/action_impl.cpp
+        err=err+$?
+	cd DronecodeSDK
+        err=err+$?
+	git submodule update --init --recursive
+        err=err+$?
+	make default 
+      	err=err+$?
+	sudo make default install
+        err=err+$?
+	cd && sudo ldconfig
+	err=err+$? 
+	echo Dronecode cloned and installed.
+        echo -------------------------------------------------------------
+        echo Installing Cmake
+        sudo apt-get -y install cmake 
+        err=err+$?
+	if [ $err -gt 0 ];
+	then
+	echo Auto Launch Scripts or DronecodeSDK failed
+	break
+	fi
+	echo -------------------------------------------------------------
+        echo All dependencies installed
+        echo -------------------------------------------------------------
+        echo Preparing to reboot 
+        echo you have 5 seconds to cancel. to cancel press Ctl+C
+        sleep 5
+        echo rebooting... 
+        sleep 1
+        sudo reboot
